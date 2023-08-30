4655731780f98e91994ed9e1ea1a30676e1c4d1e
Igor Leal <igorleal96@gmail.com>
2023-08-30 13:12:50 -0300

    versão 1.0 finalizada

--- app/src/main/java/com/example/pokedexinfo/model/Pokemon.kt
+++ app/src/main/java/com/example/pokedexinfo/model/Pokemon.kt
@@ -7,7 +7,9 @@ data class Pokemon(
 ){
     val formattedNumber ="#${ id.toString().padStart(3, '0')}"
 
-    val imgUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png"
+//    val imgUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png"
+    val imgUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"
+//    val imgUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
 }
 
 
--- app/src/main/java/com/example/pokedexinfo/view/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedexinfo/view/PokemonAdapter.kt
@@ -65,4 +65,29 @@ class PokemonAdapter(private val pokemon: Pokemon) : Item<PokemonAdapter.Pokemon
             else -> R.color.gray_400 //UNKNOWN
         }
     }
+
+    private fun getIconType(type: String): Int{
+        return when (type.uppercase()) {
+            "NORMAL" -> R.color.gray_200
+            "FIGHTING" -> R.color.red_400
+            "FLYING" -> R.color.blue_200
+            "POISON" -> R.color.lilac_200
+            "GROUND" -> R.color.orange_700
+            "ROCK" -> R.color.yellow_100
+            "BUG" -> R.color.green_500
+            "GHOST" -> R.color.purple_400
+            "STEEL" -> R.color.teal_200
+            "FIRE" -> R.color.orange_500
+            "WATER" -> R.color.blue_300
+            "GRASS" -> R.color.green_700
+            "ELECTRIC" -> R.color.yellow_500
+            "PSYCHIC" -> R.color.red_300
+            "ICE" -> R.color.teal_200
+            "DRAGON" -> R.color.blue_500
+            "DARK" -> R.color.gray_500
+            "FAIRY" -> R.color.pink_500
+            "SHADOW" -> R.color.gray_400
+            else -> R.color.gray_400 //UNKNOWN
+        }
+    }
 }
